f530793576777d7cbe3baa061636c5140132d169
Damian Zabagło <damian_zabaglo@epam.com>
2026-08-29 22:44:33 +0200

    Init Repo

---
+++ AuthenticationMicroservice.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthenticationMicroservice.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
