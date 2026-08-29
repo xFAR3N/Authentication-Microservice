7db875dba5145e42ce8721a9a77a3081fe1130a9
Damian Zabagło <damian_zabaglo@epam.com>
2026-08-29 23:01:10 +0200

    Set up entities User and RefreshToken

--- AuthenticationMicroservice.Domain/Entities/RefreshToken.cs
+++ AuthenticationMicroservice.Domain/Entities/RefreshToken.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace AuthenticationMicroservice.Domain.Entities
 {
-    public class RefreshToken
+    public class RefreshToken : BaseEntity
     {
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpiresAtUtc { get; set; }
+
+        public DateTime CreateAtUtc { get; set; } = DateTime.UtcNow;
+
+        public string? CreateByIp { get; set; }
+
+        public DateTime? RevokedAtUc { get; set; }
+
+        public string? RevokedByIp { get; set; }
+
+        public string? ReplacedByToken { get; set; }
+
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAtUtc;
+
+        public bool IsRevoked => RevokedAtUc != null;
+
+        public bool IsActive => !IsRevoked && !IsExpired;
+
+        public Guid UserId { get; set; }
+
+        public User User { get; set; } = null!;
     }
 }
--- AuthenticationMicroservice.Domain/Entities/User.cs
+++ AuthenticationMicroservice.Domain/Entities/User.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace AuthenticationMicroservice.Domain.Entities
 {
-    public class User
+    public class User : BaseEntity
     {
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "User";
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdateAtUtc { get; set; }
+
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     }
 }
